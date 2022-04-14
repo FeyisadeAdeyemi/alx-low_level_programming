f7305280cf7e1be8df8ff8de9c3d1dbbfc336824
feyisadeadeyemi <adeyemicharles@gmail.com>
2022-04-14 16:38:00 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -19,3 +19,32 @@ printf("Infinite loop incoming :(\n");
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 10)*/
+	/*{*/
+	/*  putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
