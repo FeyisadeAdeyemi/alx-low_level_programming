cc00197291a058886ca6af1d1d93106ec45b0b6b
feyisadeadeyemi <adeyemicharles@gmail.com>
2022-05-15 14:44:13 -0700

    my 100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -35,10 +35,9 @@ arr = (char *)main;
 for (i = 0; i < bytes; i++)
 
 {
-
 if (i == bytes - 1)
-																{
-																printf("%02hhx\n", arr[i]);
+}
+printf("%02hhx\n", arr[i]);
 																break;
 																}
 printf("%02hhx ", arr[i]);
